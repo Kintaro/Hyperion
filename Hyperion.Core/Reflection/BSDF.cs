51adc337bc3fe3cd06ba5d0d12363f272758a4b9
Simon Wollwage <kintaro@think-in-co.de>
2010-03-16 02:34:44 +0100

    - Worked on BSDF

--- Hyperion.Core/Reflection/BSDF.cs
+++ Hyperion.Core/Reflection/BSDF.cs
@@ -6,13 +6,14 @@ namespace Hyperion.Core.Reflection
 {
 
 
-    public class BSDF
+    public sealed class BSDF
     {
         private Normal nn;
         private Normal ng;
         private Vector sn;
         private Vector tn;
         private int nBxDFs;
+        private BxDF[] bxdfs = new BxDF[8];
 
         public readonly DifferentialGeometry dgShading;
         public readonly double Eta;
@@ -30,6 +31,58 @@ namespace Hyperion.Core.Reflection
             sn = dgShading.dpdu.Normalized;
             tn = nn % sn;
             nBxDFs = 0;
+        }
+
+        public double Pdf (Vector wo, Vector wi)
+        {
+            return Pdf (wo, wi, BxDFType.BSDF_ALL);
+        }
+
+        public double Pdf (Vector woW, Vector wiW, BxDFType flags)
+        {
+            if (nBxDFs == 0)
+                return 0.0;
+            Vector wo = WorldToLocal (woW), wi = WorldToLocal (wiW);
+            double pdf = 0.0;
+            int matchingComps = 0;
+            for (int i = 0; i < nBxDFs; ++i)
+            {
+                if (bxdfs[i].MatchesFlags (flags))
+                {
+                    ++matchingComps;
+                    pdf += bxdfs[i].Pdf (wo, wi);
+                }
+            }
+            return matchingComps > 0 ? pdf / matchingComps : 0.0;
+        }
+
+        public Vector WorldToLocal (Vector v)
+        {
+            return new Vector (v ^ sn, v ^ tn, v ^ nn);
+        }
+
+        public Vector LocalToWorld (Vector v)
+        {
+            return new Vector (sn.x * v.x + tn.x * v.y + nn.x * v.z, sn.y * v.x + tn.y * v.y + nn.y * v.z, sn.z * v.x + tn.z * v.y + nn.z * v.z);
+        }
+
+        public int NumComponents ()
+        {
+            return nBxDFs;
+        }
+
+        public int NumComponents (BxDFType flags)
+        {
+            int num = 0;
+            for (int i = 0; i < nBxDFs; ++i)
+                if (bxdfs[i].MatchesFlags (flags))
+                    ++num;
+            return num;
+        }
+
+        public void Add (BxDF bxdf)
+        {
+            bxdfs[nBxDFs++] = bxdf;
         }
     }
 }
