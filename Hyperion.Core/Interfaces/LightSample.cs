2e80cde1b4cbb51a3e12116ae819dcc52ace1d46
Simon Wollwage <kintaro@think-in-co.de>
2010-03-16 13:06:32 +0100

    - Finished VisibilityTester

--- Hyperion.Core/Interfaces/LightSample.cs
+++ Hyperion.Core/Interfaces/LightSample.cs
@@ -1,12 +1,19 @@
 
 using System;
+using Hyperion.Core.Geometry;
 
 namespace Hyperion.Core.Interfaces
 {
     public class LightSample
     {
+        public double[] uPos;
+        public double uComponent;
+
         public LightSample ()
         {
+            uPos[0] = Util.Random.NextDouble ();
+            uPos[1] = Util.Random.NextDouble ();
+            uComponent = Util.Random.NextDouble ();
         }
     }
 }
--- Hyperion.Core/Interfaces/VisibilityTester.cs
+++ Hyperion.Core/Interfaces/VisibilityTester.cs
@@ -1,13 +1,32 @@
 
 using System;
+using Hyperion.Core.Geometry;
 
 namespace Hyperion.Core.Interfaces
 {
-    public class VisibilityTester
+    public struct VisibilityTester
     {
+        public Ray Ray;
 
-        public VisibilityTester ()
+        public void SetSegment (Point p1, double eps1, Point p2, double eps2, double time)
         {
+            double dist = Util.Distance (p1, p2);
+            Ray = new Ray (p1, (p2 - p1) / dist, eps1, dist * (1.0 - eps2), time);
+        }
+
+        public void SetRay (Point p, double eps, Vector w, double time)
+        {
+            Ray = new Ray (p, w, eps, double.PositiveInfinity, time);
+        }
+
+        public Spectrum Transmittance (Scene scene, IRenderer renderer, Sample sample)
+        {
+            return renderer.Transmittance (scene, new RayDifferential (Ray), sample);
+        }
+
+        public bool Unoccluded (Scene scene)
+        {
+            return !scene.IntersectP (Ray);
         }
     }
 }
